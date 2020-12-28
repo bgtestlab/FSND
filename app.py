16ba5876d23cf06314aff1b9010bcc88d8bacd47
BoramGwon <boram@DESKTOP-U31VSCJ.localdomain>
2020-12-28 13:06:26 +0900

    Create an initial postgresDB by using flask-migrate

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -22,7 +23,7 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 # TODO: connect to a local postgresql database
-
+migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
@@ -54,6 +55,8 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+"""
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
@@ -505,6 +508,8 @@ if not app.debug:
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     app.logger.info('errors')
+
+"""
 
 #----------------------------------------------------------------------------#
 # Launch.
