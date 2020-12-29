c27b9669c20f3a91f70f407e4fa0f9486278d0a9
BoramGwon <boram@DESKTOP-U31VSCJ.localdomain>
2020-12-29 11:05:51 +0900

    Update DB model by adding relationships

--- app.py
+++ app.py
@@ -29,36 +29,50 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+    __tablename__ = 'venues'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=False)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    website = db.Column(db.String(120), nullable=False)
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(120))
+    artists = db.relationship('Show', backref='venue', cascade='all, delete-orphan')
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+    __tablename__ = 'artists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500), nullable=False)
+    facebook_link = db.Column(db.String(120), nullable=False)
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
-
-"""
+    website = db.Column(db.String(120), nullable=False)
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(120))
+    venues = db.relationship('Show', backref='artist', cascade='all, delete-orphan')
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+   __tablename__ = 'shows'
+
+   venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), primary_key=True)
+   artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), primary_key=True)
+   start_time = db.Column(db.String, nullable=False)
+   venue = db.relationship('Venue', backref='artists', cascade='all, delete-orphan')
+   artist = db.relationship('Artist', backref='venues', cascade='all, delete-orphan')
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -132,6 +146,7 @@ def search_venues():
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
   # TODO: replace with real venue data from the venues table, using venue_id
+
   data1={
     "id": 1,
     "name": "The Musical Hop",
@@ -246,6 +261,7 @@ def delete_venue(venue_id):
 @app.route('/artists')
 def artists():
   # TODO: replace with real data returned from querying the database
+
   data=[{
     "id": 4,
     "name": "Guns N Petals",
@@ -509,7 +525,6 @@ if not app.debug:
     app.logger.addHandler(file_handler)
     app.logger.info('errors')
 
-"""
 
 #----------------------------------------------------------------------------#
 # Launch.
